fb527364867f568ef1bb51bd1bbf75b65115d951
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2020-12-01 14:15:19 +0700

    chore: release v0.1

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./App.css";
+import logo from "./spacex.png";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+
+function App() {
+	return (
+		<>
+			<Router>
+				<div className="container">
+					<img
+						src={logo}
+						alt="SpaceX"
+						style={{
+							width: 300,
+							display: "block",
+							margin: "auto",
+						}}
+					/>
+					<Route exact path="/" component={Launches} />
+					<Route exact path="/launch/:flight_number" component={Launch} />
+				</div>
+			</Router>
+		</>
+	);
+}
+
+export default App;
---
+++ client/src/components/Launches.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { 
+    gql,
+    useQuery,
+} from '@apollo/client';
+
+import LaunchItem from './LaunchItem';
+import MissionKey from './MissionKey';
+
+const LAUNCHES_QUERY = gql`
+    query LaunchesQuery {
+        launches {
+            flight_number
+            mission_name
+            launch_date_local
+            launch_success
+        }
+    }
+`;
+
+function LaunchesQuery() {
+    const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Something went wrong, try again later..</p>
+
+    return data.launches.map(launch => (
+        <LaunchItem key={launch.flight_number} launch={launch} />
+    ));
+}
+
+export default function Launches() {
+    return (
+        <>
+            <h1 className="display-4 my-3">Launches</h1>
+            <MissionKey />
+            <LaunchesQuery />
+        </>
+    )
+}
